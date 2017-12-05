c49fa4bfe28ca23d6758ee9396563a4c1f907a3d
Benedikt Knabe <Benedikt.Knabe@hs-furtwangen.de>
2017-12-05 14:40:30 +0100

    asdf

---
+++ src/main/java/de/knabe/App.java
@@ -0,0 +1,16 @@
+package de.knabe;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      String input = new  Scanner(System.in).nextLine().toUpperCase();
+      
+          System.out.println(input);
+   }
+}
