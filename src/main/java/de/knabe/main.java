9e3e1999a5dd4642a5b7448417e1200a828b38bc
Benedikt Knabe <Benedikt.Knabe@hs-furtwangen.de>
2017-12-12 15:36:41 +0100

    aufgabenblatt6

---
+++ src/main/java/de/knabe/main.java
@@ -0,0 +1,17 @@
+package de.knabe;
+import static org.junit.Assert.*; 
+
+public class main {
+	
+	
+	public void test(){
+		assertTrue("Fehler bei Jan", Util.istErstesHalbjahr(1));
+		assertTrue("Fehler bei Feb", Util.istErstesHalbjahr(2));
+		assertTrue("Fehler bei März", Util.istErstesHalbjahr(3));
+		assertTrue("Fehler bei April", Util.istErstesHalbjahr(4));
+		assertTrue("Fehler bei Mai", Util.istErstesHalbjahr(5));
+		assertTrue("Fehler bei Juni", Util.istErstesHalbjahr(6));
+		assertTrue("Fehler bei Juli", Util.istErstesHalbjahr(7));	
+	}
+	
+}
---
+++ src/main/java/de/knabe/test.java
@@ -0,0 +1,18 @@
+package de.knabe;
+import static org.junit.Assert.*; 
+import org.junit.Test;
+
+
+public class test {
+	
+	@Test
+	public void test1(){
+		
+	Queue bla = new Queue(3);
+	bla.enqueue(1);
+	bla.enqueue(2);
+	bla.enqueue(3);
+	bla.enqueue(4);
+	assertEquals("Es ist nicht 3", 1, bla.dequeue());
+	}
+}
